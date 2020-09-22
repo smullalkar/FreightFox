d6df008a413925c280f2905567264eae95259e34
smullalkar <smullalkar@gmail.com>
2020-09-22 18:11:47 +0530

    all filters completed

--- src/Redux/Actions.js
+++ src/Redux/Actions.js
@@ -1,8 +1,45 @@
 import {
-    CHANGE_PAGE_NO
+    CHANGE_PAGE_NO,
+    SORT_BY_AMOUNT,
+    SORT_BY_PID,
+    SORT_BY_EMAIL,
+    FILTER_PAYMENT_STATUS,
+    FILTER_DATERANGE,
+    DELETE_PAYMENT
 } from "./Actiontypes";
 
 export const changePage = payload => ({
     type: CHANGE_PAGE_NO,
     payload
+})
+
+
+export const sortByAmount = payload => ({
+    type: SORT_BY_AMOUNT,
+    payload,
+})
+
+export const sortByPid = payload => ({
+    type: SORT_BY_PID,
+    payload,
+})
+
+export const sortByEmail = payload => ({
+    type: SORT_BY_EMAIL,
+    payload,
+})
+
+export const filterByPaymentStatus = payload => ({
+    type: FILTER_PAYMENT_STATUS,
+    payload,
+})
+
+export const filterByDateRange = payload => ({
+    type: FILTER_DATERANGE,
+    payload,
+})
+
+export const deletePayment = payload => ({
+    type: DELETE_PAYMENT,
+    payload,
 })
--- src/Redux/Reducer.js
+++ src/Redux/Reducer.js
@@ -1,5 +1,11 @@
 import {
-    CHANGE_PAGE_NO
+    CHANGE_PAGE_NO,
+    SORT_BY_AMOUNT,
+    SORT_BY_PID,
+    SORT_BY_EMAIL,
+    FILTER_PAYMENT_STATUS,
+    FILTER_DATERANGE,
+    DELETE_PAYMENT
 } from "./Actiontypes";
 import sampleData from '../Components/Data/data.json'
 
@@ -8,9 +14,11 @@ const initState = {
     page: 1,
     perPage: 5,
     length: 0,
-    totalPages: Math.ceil(sampleData.length/5),
+    totalPages: Math.ceil(sampleData.length / 5),
     isLoadng: false,
-    error: false
+    error: false,
+    filteredData: [],
+    isFilter: false
 };
 
 export default (state = initState, { type, payload }) => {
@@ -21,6 +29,69 @@ export default (state = initState, { type, payload }) => {
             return {
                 ...state,
                 page: payload
+            }
+        case SORT_BY_AMOUNT:
+            var dat1 = payload === 'asc' ? [...state.data].sort((a, b) => Number(a.amount) - Number(b.amount)) : [...state.data].sort((a, b) => Number(b.amount) - Number(a.amount))
+            return {
+                ...state,
+                data: dat1
+            }
+        case SORT_BY_PID:
+            var dat2 = payload === 'asc' ? [...state.data].sort((a, b) => Number(a.paymentId) - Number(b.paymentId)) : [...state.data].sort((a, b) => Number(b.paymentId) - Number(a.paymentId))
+            return {
+                ...state,
+                data: dat2
+            }
+        case SORT_BY_EMAIL:
+            var dat3 = payload === 'asc' ? [...state.data].sort((a, b) => a.customerEmail > b.customerEmail) : [...state.data].sort((a, b) => b.customerEmail > a.customerEmail)
+            return {
+                ...state,
+                data: dat3
+            }
+        case FILTER_PAYMENT_STATUS:
+            console.log('payload', payload)
+            if (payload === 'All') {
+                let totalPages = Math.ceil(state.data.length / state.perPage)
+                return {
+                    ...state,
+                    isFilter: false,
+                    filteredData: state.data,
+                    totalPages: totalPages
+                }
+            }
+            let filData = [...state.data].filter(item => {
+                return item.paymentStatus === payload
+            })
+            var totalPages = Math.ceil(filData.length / state.perPage)
+            return {
+                ...state,
+                isFilter: true,
+                filteredData: filData,
+                totalPages: totalPages
+            }
+        case FILTER_DATERANGE:
+            var startDate = new Date(payload[0]);
+            var endDate = new Date(payload[1]);
+
+            var resultProductData = state.data.filter(a => {
+                var date = new Date(a.orderDate);
+                return (date >= startDate && date <= endDate);
+            });
+            console.log(resultProductData)
+            var totalPages = Math.ceil(resultProductData.length / state.perPage)
+            // var dat3 = payload === 'asc' ? [...state.data].sort((a, b) => a.customerEmail > b.customerEmail) : [...state.data].sort((a, b) => b.customerEmail > a.customerEmail)
+            return {
+                ...state,
+                isFilter: true,
+                filteredData: resultProductData,
+                totalPages: totalPages
+            }
+        case DELETE_PAYMENT:
+            return {
+                ...state,
+                data: [...state.data].filter(item => {
+                    return item.paymentId !== payload
+                })
             }
         default:
             return state;
