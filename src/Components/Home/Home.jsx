424f59b910322e19cc246bd398f8ebd41a1cdfda
smullalkar <smullalkar@gmail.com>
2020-09-22 18:11:10 +0530

    filters added to home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 // import { connect } from 'react-redux'
 import Pagination from '../Pagination/Pagination'
 import Data from '../Pagination/Data'
+import Filters from '../Filters/Filters'
 
 export default function Home() {
     return (
-        <div>
+        <div className='pb-5'>
+            <Filters/>
             <Data />
             <Pagination />
         </div>
