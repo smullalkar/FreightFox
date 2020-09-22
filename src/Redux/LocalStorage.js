295207f021ae8705d41efcc70ea99f10f48391da
smullalkar <smullalkar@gmail.com>
2020-09-22 23:06:14 +0530

    set to localstorage

---
+++ src/Redux/LocalStorage.js
@@ -0,0 +1,14 @@
+export const loadData = (key) => {
+    try {
+        let data = localStorage.getItem(key);
+        data = JSON.parse(data);
+        return data;
+    } catch (err) {
+        return undefined;
+    }
+};
+
+export const saveData = (key, data) => {
+    console.log("save data");
+    localStorage.setItem(key, JSON.stringify(data));
+};
