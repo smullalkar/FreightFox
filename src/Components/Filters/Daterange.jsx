fc39efc313336b500e493bb65caa7f7bc2bf4dc9
smullalkar <smullalkar@gmail.com>
2020-09-22 23:00:43 +0530

    UI improvised

--- src/Components/Filters/Daterange.jsx
+++ src/Components/Filters/Daterange.jsx
@@ -10,14 +10,14 @@ const Daterange = ({
     const [value, onChange] = useState([new Date(), new Date()]);
     console.log(value)
     return (
-        <div>
-            <h5>Filter by Date Range</h5>
+        <div className='mt-lg-3 mt-md-3 mt-sm-2 mt-2 col-sm-10 col-10 col-md-12 col-lg-12'>
+            <h3>FILTER BY DATE RANGE</h3>
             <DateRangePicker
                 onChange={onChange}
                 value={value}
             />
             <button
-                className='btn btn-dark mx-3'
+                className='btn btn-dark m-3 mx-sm-3 mx-md-3 mx-lg-3'
                 onClick={() => filterByDateRange(value)}
             >SUBMIT</button>
         </div>
