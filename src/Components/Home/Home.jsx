a2972fe60dd5ae7fa6b3c724ae9cc2dc16282244
smullalkar <smullalkar@gmail.com>
2020-09-22 12:27:48 +0530

    Home component created

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { connect } from 'react-redux'
+import Pagination from '../../Pagination/Pagination'
+import Data from '../../Pagination/Data'
+
+export default function Home() {
+    return (
+        <div>
+            <Pagination />
+            <Data />
+        </div>
+    )
+}
